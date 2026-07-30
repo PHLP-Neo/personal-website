37836460a768af616d291693b5a1b7125829d37c
PHLP-Neo <phlp.neo@gmail.com>
2026-07-31 03:13:33 +1000

    Created admin account

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "featured",
+        "display_order",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "status",
+        "featured",
+    )
+
+    search_fields = (
+        "title",
+        "short_description",
+        "description",
+        "technologies",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+    ordering = (
+        "display_order",
+        "-created_at",
+    )
