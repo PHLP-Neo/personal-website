6cd793dfdd8f49b0a03a96489cdffbfb8cb9e9aa
PHLP-Neo <phlp.neo@gmail.com>
2026-07-31 03:44:07 +1000

    Remove the old Core Notes route

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,5 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("notes/", views.notes, name="notes"),
     path("contact/", views.contact, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -21,9 +21,5 @@ def about(request):
     return render(request, "core/about.html")
 
 
-def notes(request):
-    return render(request, "core/notes.html")
-
-
 def contact(request):
     return render(request, "core/contact.html")
