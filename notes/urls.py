0590d2b1d6b8648cb4d5ab5e51f6f42a2b7f2d17
PHLP-Neo <phlp.neo@gmail.com>
2026-07-31 03:42:30 +1000

    Create Notes URLs

---
+++ notes/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = "notes"
+
+urlpatterns = [
+    path(
+        "",
+        views.post_list,
+        name="list",
+    ),
+    path(
+        "<slug:slug>/",
+        views.post_detail,
+        name="detail",
+    ),
+]
