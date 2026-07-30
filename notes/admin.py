71aa5b9b21fd0bf11177043c1b74786d1a7cd752
PHLP-Neo <phlp.neo@gmail.com>
2026-07-31 03:41:38 +1000

    Register posts in Django Admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "published",
+        "published_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "published",
+        "published_at",
+    )
+
+    search_fields = (
+        "title",
+        "summary",
+        "body",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+    ordering = ("-published_at",)
